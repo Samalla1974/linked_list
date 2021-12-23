1a469a0caf702e72ccb5bee90fe27a545650c86c
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-23 19:17:14 +0530

    Add files via upload

---
+++ MyNode.java
@@ -0,0 +1,20 @@
+package java_linkedlist;
+
+public class MyNode<k> implements java_linkedlist.INode<k>
+{
+	private k key;
+	private INode next;
+	
+	public MyNode(k key) {
+		this.key = key;
+		this.next = null;
+	}
+	
+	public INode getNext() {
+		return next;
+	}
+	
+	public void setNext(INode next) {
+		this.next = next;
+	}
+}
