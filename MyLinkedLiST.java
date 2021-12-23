d8defaa17ed7e3b1e9d28304ffe56b0c515e04c8
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-23 19:39:57 +0530

    Add files via upload

--- MyLinkedLiST.java
+++ MyLinkedLiST.java
@@ -7,21 +7,9 @@ private INode tail;
 
 public MyLinkedList() {
 	this.head = null;
-	this.tail = null;
-
-public void add (INode MyNode) {
-	if (this.tail ==  null) {
-	this.tail = newNode;
-	}
-	if(this.head == my) {
-	this.head = newNode;	
-	} else {
-	this.head = newNode;
-	this.head.setNext((tempNode));
-
+    this.tail = null;
 }
-	
-}
+
 
 public void printMyNodes() {
 	StringBuffer myNodes = new StringBuffer("My Nodes: ");
