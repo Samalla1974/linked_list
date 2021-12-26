3ef2cc7f93c8084ceb36f1860092b7121a3ddd08
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-26 14:06:27 +0530

    Add files via upload

---
+++ MyLInkedListTest.java
@@ -0,0 +1,18 @@
+package java_linkedlist;
+
+public class MyLInkedListTest {
+	public static void main (String args[])
+	{
+
+	MyNode<Integer> myFirstNode = new MyNode<>( key:56);
+	MyNode<Integer> myThirdNode = new MyNode<>( key:70);
+	MyNode<Integer> mySeondNode = new MyNode<>( key:30);
+	MyLinkedListTest MyLinkedList  = new MyLinkedListTest();
+	MyLinkedLiST.add(myFirstNode);
+	MyLinkedLiST.push(mySecondNode);
+	MyLinkedLiST.add(myThirdNode);
+	MyLinkedLiST.printMyNodes();
+	
+}
+
+}
